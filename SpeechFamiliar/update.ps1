e478ff56ea871a962e5c5e4a8db4aa32e5dcc06d
silentorb <christopherwjohnson@gmail.com>
2013-08-20 21:54:59 -0600

    Updating the code and adding missing dlls.

--- SpeechFamiliar/update.ps1
+++ SpeechFamiliar/update.ps1
@@ -13,7 +13,6 @@ copy "E:\Programs\MetaHub\*.dll" "E:\Programs\speechfamiliar\MetaHub"
 copy "E:\Programs\MetaHub\MetaHub.exe" "E:\Programs\speechfamiliar\MetaHub"
 copy "E:\Programs\MetaHub\plugins\*.dll" "E:\Programs\speechfamiliar\MetaHub\plugins"
 copy "E:\Programs\MetaHub\global\*.*" "E:\Programs\speechfamiliar\MetaHub\global"
-copy "E:\Programs\MetaHub\xsl\*.*" "E:\Programs\speechfamiliar\MetaHub\xsl"
 
 &"E:\Programs\SpeechFamiliar\bin\SpeechFamiliar.exe"
 sleep -s 10
